cd3b5fd03a4b79986fa7dc9e6bd6f97d320cbafb
Alfredo Roca <alfredo.roca.mas@gmail.com>
2015-11-09 10:48:11 +0100

    changed destination forlder for deplyment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,5 @@
-server '67.205.57.114', user: 'alfredo', roles: %w{web app}, primary: true, port: 53100
-set :deploy_to, '/home/alfredo/rails_apps/blog_67webs'
+server '67.205.57.114', user: 'deployer', roles: %w{web app}, primary: true, port: 53100
+set :deploy_to, '/var/www/blog_67webs'
 set :use_sudo, false
 set :rvm_ruby_version, '2.2.0@blog_67webs'
 set :rake_env, 'staging'
