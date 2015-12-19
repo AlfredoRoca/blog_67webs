f8b355c03a8591a4b0f76fd7cfdc2af18323798a
Alfredo Roca <alfredo.roca.mas@gmail.com>
2015-12-19 22:22:45 +0100

    added disqus for comments

--- js/disqus.js
+++ js/disqus.js
@@ -2,10 +2,10 @@
  * CONFIGURATION VARIABLES : https://disqus.com/admin/universalcode/#configuration-variables
  */
 var disqus_shortname = 'blog67webs';
-// var disqus_config = function () {
-//     this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-//     this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-// };
+var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
 (function() {  // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
     
